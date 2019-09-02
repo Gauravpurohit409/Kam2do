c3d7b3aee104a43198822037113519fbf8ec962e
piedcipher <pateltirth454@gmail.com>
2019-09-02 17:25:23 +0530

    added ToDo Model

--- kam2do/models.py
+++ kam2do/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    title = models.TextField
+    description = models.TextField
+    isCompleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str({
+            'title': self.title,
+            'description': self.description,
+            'isCompleted': self.isCompleted
+        })
