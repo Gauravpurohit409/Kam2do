dd72ac235a1489a14157beb7272e452bde943bdd
piedcipher <pateltirth454@gmail.com>
2019-09-05 18:28:19 +0530

    Deleted Everything, Django

--- kam2do/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Kam2DoConfig(AppConfig):
-    name = 'kam2do'
--- kam2do/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from kam2do import views
-app_name = 'kam2do'
-
-urlpatterns = [
-		path('',views.index,name = 'index'),
-]
-
-
